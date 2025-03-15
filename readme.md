cbcbdfafac4c197cac9c1c20a3952ce2916dc1db
Chiaope <gui_kang@hotmail.com>
2025-03-15 21:42:22 +0800

    initial push

---
+++ readme.md
@@ -0,0 +1,14 @@
+# GIC Cinemas Booking System
+
+## Requirements
+
+- Python 3.11
+
+## Installation
+
+Clone the repository and install the required dependencies:
+
+```bash
+git clone https://github.com/yourusername/gic-cinemas-booking-system.git
+cd gic-cinemas-booking-system
+pip install -r requirements.txt"# GIC_Cinema" 
---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+class TestCinema(unittest.TestCase):
+    def test_movie_initiated(self):
+        pass
+
+    def test_invalid_movie_config_handled(self):
+        pass
+
+    def test_invalid_number_of_ticket_entered(self):
+        pass
+
+    def test_valid_number_of_ticket_entered(self):
+        pass
+
+    def test_number_of_ticket_overflow(self):
+        pass
+
+    def test_number_of_tickets_more_than_seas(self):
+        pass
+
+    def test_additional_invalid_number_of_ticket_entered(self):
+        pass
+
+    def test_additional_valid_number_of_ticket_entered(self):
+        pass
+
+    def test_invalid_manual_seat_selection(self):
+        pass
+
+    def test_valid_manual_seat_selection(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
